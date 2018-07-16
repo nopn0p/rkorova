a8816f44fc00704e509b2b91a7fc8b2e3093a070
nathan <nathan@bandm.bargains>
2018-07-16 21:38:37 +0100

    Update xor.py

--- xor.py
+++ xor.py
@@ -1,10 +1,6 @@
-import crypt
+import sys
 
 def xor(string):
-    return ''.join(list('\\x' + hex(ord(x) ^ 0x2A)[2:] for x in string))
+    return ''.join(list('\\x' + hex(ord(x) ^ 0x2A)[2:] for x in sys.argv[1]))
 
-def main(): 
-    print("simple little xor util, included in rkorova for your convenience")
-    word = input("word: ") 
-    print(xor(word))
-main()
+xor()
