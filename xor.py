521d1d4305ab3809486fcb26cf9ecd41964a1372
nathan <nathan@bandm.bargains>
2018-07-23 16:25:36 +0100

    Update xor.py

--- xor.py
+++ xor.py
@@ -1,6 +1,3 @@
 import sys
 
-def xor():
-    return ''.join(list('\\x' + hex(ord(x) ^ 0x2A)[2:] for x in sys.argv[1]))
-
-xor()
+print(''.join(list('\\x' + hex(ord(x) ^ 0x2A)[2:] for x in sys.argv[1]))
